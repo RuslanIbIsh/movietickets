5da5c059503686bf8e156759a908452338645592
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2021-02-04 12:19:17 +0200

    first mentor suggestion impl

--- src/main/java/com/iri/movietickets/model/Ticket.java
+++ src/main/java/com/iri/movietickets/model/Ticket.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -15,7 +16,7 @@ public class Ticket {
     private Long id;
     @OneToOne
     private MovieSession movieSession;
-    @OneToOne
+    @ManyToOne
     private User user;
 
     public Long getId() {
